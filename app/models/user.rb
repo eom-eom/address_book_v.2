d022dc881807ac78815d71ab0df65179bc92c41e
eom-eom <elbertmolina_25@hotmail.com>
2015-09-04 09:28:00 +0800

    devise views added

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def apply_omniauth(auth)
+    # binding.pry
+    # In previous omniauth, 'user_info' was used in place of 'raw_info'
+    self.email = "#{auth['extra']['raw_info']['name'].split(" ").first.downcase}@spark.com"
+    # Again, saving token is optional. If you haven't created the column in authentications table, this will fail
+    authentications.build(:provider => auth['provider'], :uid => auth['uid'], :token => auth['credentials']['token'])
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
